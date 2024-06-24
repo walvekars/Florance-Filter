d89b54c4c2c9611bb9aa0304117059901e9b6cfb
ramdas409 <dasari.rammohan@primemindsconsulting.com>
2024-06-24 16:03:50 +0530

    testin2

--- google_chat/controllers/controllers.py
+++ google_chat/controllers/controllers.py
@@ -213,7 +213,7 @@ class LivechatChatbotScriptController(http.Controller):
                 creds_val=json.loads(data_response.text)
                 print('creds.to_json()',data_response.text)
 
-                creds_val.update({'refresh_token':creds_val['access_token'],'client_id':clients_data['web']['client_id'],'client_secret':clients_data['web']['client_secret'],"universe_domain": "googleapis.com", "account": "","token_uri": "https://oauth2.googleapis.com/token"})
+                creds_val.update({'refresh_token':creds_val['token'],'client_id':clients_data['web']['client_id'],'client_secret':clients_data['web']['client_secret'],"universe_domain": "googleapis.com", "account": "","token_uri": "https://oauth2.googleapis.com/token"})
                 url.write(json.dumps(creds_val))
 
         # wsgi_app = _RedirectWSGIApp("Authenticated")
